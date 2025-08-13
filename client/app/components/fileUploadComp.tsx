70aefe8e474c6cb838a82e028d601952f911d5a1
Akshat <codepreneur.in@gmail.com>
2025-08-13 22:38:30 +0530

    fix/fileupload

--- client/app/components/fileUploadComp.tsx
+++ client/app/components/fileUploadComp.tsx
@@ -1,25 +1,39 @@
 "use client"
-import {Upload} from "lucide-react";
+import { Upload } from "lucide-react";
 
-export default function FileUploadComp(){
+export default function FileUploadComp() {
 
-    const handlefileUploadButton=()=>{
+    const handlefileUploadButton = () => {
         const el = document.createElement("input");
         el.setAttribute("type", "file");
         el.setAttribute("accept", "application/pdf");
-        el.addEventListener("change",(ev)=>{
-            if(el.files && el.files.length>0){
+        el.addEventListener("change", async (ev) => {
+            if (el.files && el.files.length > 0) {
                 const file = el.files.item(0);
+                if (file) {
+                    const formData = new FormData()
+                    formData.append("pdf", file)
+
+                    await fetch("http://localhost:3000/upload/pdf",{
+                        method:"POST",
+                        body:formData
+                    })
+                    console.log(formData);
+                    
+                    console.log("File Uploaded");
+                    
+                }
+
             }
         });
         el.click();
     }
-    return(
+    return (
         <div className="bg-slate-900 text-white shadow-2xl flex justify-center content-center items-center p-4 rounded-lg">
             <div onClick={handlefileUploadButton} className=" flex justify-center h-[20vw] items-center flex-col " >
-            <h3>Upload Your PDF file </h3>
+                <h3>Upload Your PDF file </h3>
             </div>
-            
+
             <Upload />
         </div>
     )
--- server/index.js
+++ server/index.js
@@ -1,14 +1,25 @@
 import express from "express";
-
+import multer from "multer";
+const upload = multer({ dest: "uploads/" })
 const app = express();
 const PORT = 8000
 
-app.get("/",(req,res)=>{
-    
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename:function (res,file,cb){
+        const uniqueSuffix = Date.now()+"-"+Math.round(Math.round()*1e9);
+        cb(null,`${uniqueSuffix}-${file.originalname}`)
+    },
+})
+
+app.post("/upload/pdf", upload.single("pdf"), (req, res) => {
+    return res.json({ message: "uploaded" })
 
 })
 
-app.listen(PORT, (err)=>{
+app.listen(PORT, (err) => {
     console.log(`Server Started on PORT :${PORT}`);
-    
+
 })
