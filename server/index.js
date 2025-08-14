b8ec32412ede54000ae1b316b898afce905a53e2
Akshat <codepreneur.in@gmail.com>
2025-08-14 10:53:35 +0530

    added multer

--- server/index.js
+++ server/index.js
@@ -1,25 +1,33 @@
 import express from "express";
 import multer from "multer";
-const upload = multer({ dest: "uploads/" })
+import cors from "cors"
+
+
+
 const app = express();
+app.use(cors())
 const PORT = 8000
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, 'uploads/');
     },
-    filename:function (res,file,cb){
-        const uniqueSuffix = Date.now()+"-"+Math.round(Math.round()*1e9);
-        cb(null,`${uniqueSuffix}-${file.originalname}`)
+    filename: function (res, file, cb) {
+        const uniqueSuffix = Date.now() + "-" + Math.round(Math.round() * 1e9);
+        cb(null, `${uniqueSuffix}-${file.originalname}`)
     },
 })
-
+const upload = multer({storage:storage})
 app.post("/upload/pdf", upload.single("pdf"), (req, res) => {
     return res.json({ message: "uploaded" })
 
 })
 
 app.listen(PORT, (err) => {
+    if (err) {
+        console.log(err);
+
+    }
     console.log(`Server Started on PORT :${PORT}`);
 
 })
